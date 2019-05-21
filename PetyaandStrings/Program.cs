b676b03bffd9c8f67c5340bc515b3b9dbda94024
James Absalem Lintu <26407837+jamesabsalem@users.noreply.github.com>
2019-05-21 20:16:45 +0600

    first commit

---
+++ PetyaandStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PetyaandStrings
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var x = Console.ReadLine().ToLower();
+            var y = Console.ReadLine().ToLower();
+            if (string.CompareOrdinal(x, y) < 0)
+                Console.WriteLine(-1);
+            if (string.CompareOrdinal(x, y) == 0)
+                Console.WriteLine(0);
+            if (string.CompareOrdinal(x, y) > 0)
+                Console.WriteLine(1);
+        }
+    }
+}
